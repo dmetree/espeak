55f2caca1c6aff90eb0198fa7499b5aa5cb8c1e5
Anastasia Polozova <polozova.anastasi@gmail.com>
2025-11-11 07:55:10 -0300

    ui fixes for the first screen

---
+++ components/pages/Dashboard/mock.ts
@@ -0,0 +1,29 @@
+export const availableLessons = [
+  {
+    id: 1,
+    title: 'English lesson',
+    teacher: 'Adam Smith',
+    date: '05 March',
+    timeStart: '10:00',
+    timeEnd: '11:00',
+    link: '#',
+  },
+  {
+    id: 2,
+    title: 'English lesson',
+    teacher: 'Adam Smith',
+    date: '14 March',
+    timeStart: '14:00',
+    timeEnd: '15:00',
+    link: '#',
+  },
+  {
+    id: 3,
+    title: 'English lesson',
+    teacher: 'Adam Smith',
+    date: '22 March',
+    timeStart: '09:00',
+    timeEnd: '10:00',
+    link: '#',
+  },
+];
