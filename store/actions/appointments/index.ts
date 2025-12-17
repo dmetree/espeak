bada7dfccb41afcb12caaf60b9aa3a050b02eed6
adakonda <adakondacoin@gmail.com>
2025-12-17 10:54:54 +0100

    task adjusted acceptance

--- store/actions/appointments/index.ts
+++ store/actions/appointments/index.ts
@@ -297,13 +297,13 @@ export const createAppointment =
     singletonId?: string,
     txId?: any,
     partnerOne?: string,
-    partnerTwo?: string
+    partnerTwo?: string,
+    studentWallet?: string
   ) =>
   async (dispatch: Dispatch, getState: () => any) => {
     dispatch(createAppointmentStart());
 
-    const { user } = getState();
-    const studentWallet = user?.userData?.walletAddress;
+    console.log("Adding  studentWallet to appointment", studentWallet);
 
     if (!userUid) {
       const error = new Error("Cannot create appointment: no user ID.");
